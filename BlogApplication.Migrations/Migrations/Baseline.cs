c8194146cc9e524254fd1d3fb677562e804163d4
Oscar Steer <ohcsteer@gmail.com>
2019-05-01 11:07:19 +0930

    Added Baseline Migration

--- BlogApplication.Migrations/Migrations/Baseline.cs
+++ BlogApplication.Migrations/Migrations/Baseline.cs
@@ -13,12 +13,38 @@ namespace BlogApplication.Migrations.Migrations
     {
         public override void Up()
         {
+            Create.Table("User")
+                .WithId()
+                .WithColumn("Username").AsString(255).Unique().NotNullable()
+                .WithColumn("EmailAddress").AsString(255).Unique().NotNullable()
+                .WithColumn("Password").AsMaxString().NotNullable()
+                .WithColumn("FirstName").AsString(255).Nullable()
+                .WithColumn("LastName").AsString(255).Nullable()
+                .WithColumn("DateOfBirth").AsDateTime().Nullable()
+                .WithColumn("CreatedDate").AsDateTime().NotNullable()
+                .WithColumn("DateInactive").AsDateTime().Nullable();
 
+            Create.Table("BlogPost")
+                .WithId()
+                .WithColumn("UserId").AsInt32().ForeignKey("User", "Id")
+                .WithColumn("Title").AsString(100).NotNullable()
+                .WithColumn("Body").AsMaxString().NotNullable()
+                .WithColumn("PublishDate").AsDateTime().NotNullable();
+
+            Create.Table("Comment")
+                .WithId()
+                .WithColumn("UserId").AsInt32().ForeignKey("User", "Id")
+                .WithColumn("BlogPostId").AsInt32().ForeignKey("BlogPost", "Id")
+                .WithColumn("Title").AsString(100).NotNullable()
+                .WithColumn("Body").AsString(255).NotNullable()
+                .WithColumn("PostedDate").AsDateTime().NotNullable();
         }
 
         public override void Down()
         {
-
+            Delete.Table("Comment");
+            Delete.Table("BlogPost");
+            Delete.Table("User");
         }
     }
 }
